0fd652cabcf9e8ca75d885b5f62cb3723c95d8e3
OlvMrn <olivier.morin91@gmail.com>
2023-10-26 12:10:31 +0200

    insertion de données dans la table item

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -2,3 +2,5 @@ create table item (
   id int unsigned primary key auto_increment not null,
   title varchar(255) not null
 );
+
+INSERT INTO item (title) VALUES ("100Pressions"),("Catégories"), ("Prix");
