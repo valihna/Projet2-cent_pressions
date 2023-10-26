f782b9c00c778afd744f614952a26bcbaf9bffdd
OlvMrn <olivier.morin91@gmail.com>
2023-10-26 14:43:41 +0200

    Installation d'axios. Mise en place de la requete, log.info en attendant correction du CORS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const load = () => {
+    axios.get("http://localhost:3310/api/items").then((items) => {
+      console.info(items);
+    });
+  };
+
   return (
     <div className="App">
-      <button type="button">Load</button>
+      <button type="button" onClick={load}>
+        Load
+      </button>
     </div>
   );
 }
