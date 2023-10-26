fad235967c9925dad8138ea6cec8ecc7fdd00cbd
OlvMrn <olivier.morin91@gmail.com>
2023-10-26 12:16:24 +0200

    mise en place de la route items

--- backend/src/router.js
+++ backend/src/router.js
@@ -7,16 +7,17 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Import itemControllers module for handling item-related operations
-const itemControllers = require("./controllers/itemControllers");
 
 // Route to get a list of items
-router.get("/items", itemControllers.browse);
+router.get("/items", (req, res) => {
+  res.send("bonjour toto");
+});
 
 // Route to get a specific item by ID
-router.get("/items/:id", itemControllers.read);
+// router.get("/items/:id", itemControllers.read);
 
 // Route to add a new item
-router.post("/items", itemControllers.add);
+// router.post("/items", itemControllers.add);
 
 /* ************************************************************************* */
 
