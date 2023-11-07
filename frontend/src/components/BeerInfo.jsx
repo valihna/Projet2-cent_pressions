a957e664929c813ca774a8247efb6803777994fe
OlvMrn <olivier.morin91@gmail.com>
2023-11-07 21:35:21 +0100

    Modification du style de la page informations sur la bière selectionnée

--- frontend/src/components/BeerInfo.jsx
+++ frontend/src/components/BeerInfo.jsx
@@ -7,18 +7,14 @@ function BeerInfo() {
   return (
     <div className="beer-info">
       <h1>More informations</h1>
+      <div className="beer-info-title">
+        <h2>{selectedBeer.name}</h2>
+        <h3>"{selectedBeer.tagline}"</h3>
+      </div>
       <div className="beer-info-body">
         <div className="beer-info-body-left">
-          <div className="beer-info-title">
-            <h2>{selectedBeer.name}</h2>
-            <h3>"{selectedBeer.tagline}"</h3>
-          </div>
           <table className="beer-info-table">
             <tbody>
-              <tr>
-                <td>Name:</td>
-                <td>{selectedBeer.name}</td>
-              </tr>
               <tr>
                 <td>Type:</td>
                 <td>{selectedBeer.type}</td>
@@ -28,12 +24,21 @@ function BeerInfo() {
                 <td>{selectedBeer.description}</td>
               </tr>
               <tr>
+                <td>Brewery tips:</td>
+                <td>{selectedBeer.brewers_tips}</td>
+              </tr>
+              <tr>
                 <td>First brewed date:</td>
                 <td>{selectedBeer.first_brewed}</td>
               </tr>
               <tr>
-                <td>Brewery tips:</td>
-                <td>{selectedBeer.brewers_tips}</td>
+                <td>Brewed by:</td>
+                <td>
+                  {selectedBeer.contributed_by.substring(
+                    0,
+                    selectedBeer.contributed_by.indexOf(" <")
+                  )}
+                </td>
               </tr>
               <tr>
                 <td>ABV:</td>
@@ -52,10 +57,6 @@ function BeerInfo() {
               <tr>
                 <td>Price per liter:</td>
                 <td>{selectedBeer.price_per_liter} $</td>
-              </tr>
-              <tr>
-                <td>Brewed by:</td>
-                <td>{selectedBeer.contributed_by}</td>
               </tr>
             </tbody>
           </table>
