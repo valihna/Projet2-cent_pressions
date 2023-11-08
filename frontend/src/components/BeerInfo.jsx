31f75e3ae7dfb2618159aaf6a60b254b09502529
OlvMrn <olivier.morin91@gmail.com>
2023-11-08 15:03:58 +0100

    Page beers et composant Beerinfo qui permet d'afficher les données d'une biere fonctionnel, css et responsive OK

--- frontend/src/components/BeerInfo.jsx
+++ frontend/src/components/BeerInfo.jsx
@@ -7,59 +7,70 @@ function BeerInfo() {
   return (
     <div className="beer-info">
       <h1>More informations</h1>
-      <div className="beer-info-title">
-        <h2>{selectedBeer.name}</h2>
-        <h3>"{selectedBeer.tagline}"</h3>
+      <div className={`beer-info-title bg-${selectedBeer.type}`}>
+        <div className="beer-info-title-left">
+          <h2>{selectedBeer.name}</h2>
+          <h3>"{selectedBeer.tagline}"</h3>
+        </div>
+        <img
+          className="beer-info-image-mini"
+          src={selectedBeer.image_url}
+          alt={selectedBeer.name}
+        />
       </div>
       <div className="beer-info-body">
         <div className="beer-info-body-left">
-          <table className="beer-info-table">
-            <tbody>
-              <tr>
-                <td>Type:</td>
-                <td>{selectedBeer.type}</td>
-              </tr>
-              <tr>
-                <td>Description:</td>
-                <td>{selectedBeer.description}</td>
-              </tr>
-              <tr>
-                <td>Brewery tips:</td>
-                <td>{selectedBeer.brewers_tips}</td>
-              </tr>
-              <tr>
-                <td>First brewed date:</td>
-                <td>{selectedBeer.first_brewed}</td>
-              </tr>
-              <tr>
-                <td>Brewed by:</td>
-                <td>
-                  {selectedBeer.contributed_by.substring(
-                    0,
-                    selectedBeer.contributed_by.indexOf(" <")
-                  )}
-                </td>
-              </tr>
-              <tr>
-                <td>ABV:</td>
-                <td>{selectedBeer.abv}% vol.</td>
-              </tr>
-              <tr>
-                <td>IBU:</td>
-                <td>{selectedBeer.ibu}</td>
-              </tr>
-              <tr>
-                <td>Volume:</td>
-                <td>
-                  {selectedBeer.volume_value} {selectedBeer.volume_unit}
-                </td>
-              </tr>
-              <tr>
-                <td>Price per liter:</td>
-                <td>{selectedBeer.price_per_liter} $</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="beer-info-content">
+            <div className="beer-info-content-header">Type :</div>
+            <div className="beer-info-content-value">{selectedBeer.type}</div>
+          </div>
+          <div className="beer-info-content">
+            <div className="beer-info-content-header">Description:</div>
+            <div className="beer-info-content-value">
+              {selectedBeer.description}
+            </div>
+          </div>
+          <div className="beer-info-content">
+            <div className="beer-info-content-header">Brewery tips:</div>
+            <div className="beer-info-content-value">
+              {selectedBeer.brewers_tips}
+            </div>
+          </div>
+          <div className="beer-info-content">
+            <div className="beer-info-content-header">First brewed date:</div>
+            <div className="beer-info-content-value">
+              {selectedBeer.first_brewed}
+            </div>
+          </div>
+          <div className="beer-info-content">
+            <div className="beer-info-content-header">Brewed by:</div>
+            <div className="beer-info-content-value">
+              {selectedBeer.contributed_by.substring(
+                0,
+                selectedBeer.contributed_by.indexOf(" <")
+              )}
+            </div>
+          </div>
+          <div className="beer-info-content">
+            <div className="beer-info-content-header">ABV:</div>
+            <div className="beer-info-content-value">{selectedBeer.abv}</div>
+          </div>
+          <div className="beer-info-content">
+            <div className="beer-info-content-header">IBU:</div>
+            <div className="beer-info-content-value">{selectedBeer.ibu}</div>
+          </div>
+          <div className="beer-info-content">
+            <div className="beer-info-content-header">Volume:</div>
+            <div className="beer-info-content-value">
+              {selectedBeer.volume_value} {selectedBeer.volume_unit}
+            </div>
+          </div>
+          <div className="beer-info-content">
+            <div className="beer-info-content-header">Price per liter:</div>
+            <div className="beer-info-content-value">
+              {selectedBeer.price_per_liter} $
+            </div>
+          </div>
         </div>
         <div className="beer-info-body-right">
           <img
