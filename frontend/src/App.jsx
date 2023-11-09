22495e7c2bcd11880f73c9631c0274b3cddbd1f7
alain le moal <wolfdu28@aol.com>
2023-11-09 16:20:30 +0100

    Netoyage de app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,9 @@
-import axios from "axios";
 import "./App.css";
 
 function App() {
-  const load = () => {
-    axios
-      .get("http://localhost:3310/api/items")
-      .then((items) => {
-        console.info(items.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
-
   return (
     <div className="App">
-      <button type="button" onClick={load}>
-        Load
-      </button>
+      <h1>Ici va mon formulaire</h1>
     </div>
   );
 }
