84e56740478b751202bed06709967e40dbbc53f2
OlvMrn <olivier.morin91@gmail.com>
2023-11-10 15:56:02 +0100

    Modification structure du fichier

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,10 @@
 import "./Navbar.css";
-import logoSite from "../assets/logo.png";
 
 function NavBar() {
   return (
     <div className="navbar">
       <div className="navbar-header">
         <div className="logo-site">LOGO</div>
-        <img src={logoSite} alt="le logo du site" />
         <h1 className="title-site">CENT PRESSIONS</h1>
       </div>
       <div className="navbar-menu">
