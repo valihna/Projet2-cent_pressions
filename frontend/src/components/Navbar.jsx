2746b592975bd124f07a7e56e0c740d5052d877c
OlvMrn <olivier.morin91@gmail.com>
2023-11-10 17:24:46 +0100

    Modification du comportement de la navbar au scroll

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,32 @@
+import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 
 function NavBar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      setIsScrolled(scrollTop > 150);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="navbar">
       <div className="navbar-header">
-        <div className="logo-site">LOGO</div>
         <h1 className="title-site">CENT PRESSIONS</h1>
       </div>
-      <div className="navbar-menu">
-        <div className="home">HOME</div>
-        <div className="logo-cart">PANIER</div>
+      <div className={isScrolled ? "navbar-menu sticky" : "navbar-menu"}>
+        <Link className="home" to="/">
+          <h2>HOME</h2>
+        </Link>
       </div>
     </div>
   );
