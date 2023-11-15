40f87ef57045182a2bfe1a61f2af1f0d55e6e490
alain le moal <wolfdu28@aol.com>
2023-11-15 10:32:16 +0100

    mise enplace de la route de la page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import App from "./App";
+import ContactPage from "./pages/ContactPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />,
   },
 ]);
 
