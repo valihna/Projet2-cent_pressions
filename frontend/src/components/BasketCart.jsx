9d7fa736aeef566af883e2126a2da695d491ed8b
Valeriane SUDRE CHEVALIER <valeriane.chevalier@gmail.com>
2023-11-16 17:02:24 +0100

    modification pour PR retrait du code mort, ajoutement du css par rapport au reste des cartes pas de franglais

--- frontend/src/components/BasketCart.jsx
+++ frontend/src/components/BasketCart.jsx
@@ -1,54 +1,35 @@
 import { useLoaderData } from "react-router-dom";
-import NavBar from "./Navbar";
+
 import "./BasketCart.css";
 
 import Button from "./Button";
 
-// const beersInBasket = [
-//   {
-//     beerId: 1,
-//     quantity: 1,
-//   },
-//   {
-//     beerId: 5,
-//     quantity: 1,
-//   },
-//   {
-//     beerId: 25,
-//     quantity: 1,
-//   },
-// ];
-
-// Mettre en place un state pour mémoriser les bières dans le panier
 function BasketCart() {
   const cart = useLoaderData();
 
   return (
-    <div className="basketBody">
-      <div>
-        <NavBar />
-      </div>
+    <div className="basket-body">
       <div>
         <h2 className="basket">My Cart</h2>
       </div>
 
       {cart.map((beer) => (
-        <div key={beer.beerId} className="beerOne">
-          <div className="beerOneLeft">
+        <div key={beer.beerId} className="beer-one">
+          <div className="beer-one-left">
             <img src={beer.image_url} alt={beer.name} />
           </div>
-          <div className="beerOneRight">
+          <div className="beer-one-right">
             <h3 className="name">{beer.name}</h3>
             <p className="price">${beer.price_per_liter}</p>
-            <p className="sous-total">Sous-total</p>
+            <p className="subtotal">Sous-total</p>
           </div>
         </div>
       ))}
       <div className="basketFooter">
-        <div className="totalContainer">
+        <div className="total-container">
           <p className="total">Total</p>
           <span>$</span>
-          <div className="basFooter">
+          <div className="down-footer">
             <Button />
           </div>
         </div>
@@ -58,10 +39,3 @@ function BasketCart() {
 }
 
 export default BasketCart;
-
-// Mettre en place un useEffect pour charger en automatique les données relatives au bière
-// Pour se faire, utiliser axios
-// Mettre en place un route backend /basket?beers=1|5|25 (id venant de la variable beerInbasket)
-
-// En backend sur ma route, je récupère req.query.beers (eq. 1|5|25). Utiliser split('|') pour récupérer un tableau d'id puis faire une requete select ..... from .... where id IN ?, [montableau]
-// SELECT * FROM beer WHERE id in (1,5,25) LIMIT 100
--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -1,10 +1,11 @@
-// import { useState } from "react";
 import React from "react";
 import BasketCart from "../components/BasketCart";
+import Navbar from "../components/Navbar";
 
 function Basket() {
   return (
     <div>
+      <Navbar />
       <BasketCart />
     </div>
   );
